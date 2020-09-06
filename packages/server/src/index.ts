39a5330375f16b2fa3d41da91d89bde135614a2e
João Victor <jv782063@gmail.com>
2020-09-06 12:12:34 -0300

    feat(server): creates connection file and also makes the server run

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,2 +1,13 @@
 import 'reflect-metadata';
-console.log('Hello iActivities!!!');
+import './database/connection';
+import * as express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Running server in http://localhost:3333...');
+})
+
+app.listen(3333)
